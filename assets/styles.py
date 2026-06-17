8317c1a5633c606d571e04e5acea91fa75a0241d
arssenic <ayushkumar@iitbhilai.ac.in>
2026-06-17 16:00:44 +0530

    styling

---
+++ assets/styles.py
@@ -0,0 +1,17 @@
+def load_css():
+
+    return """
+    <style>
+
+    .main {
+        padding-top:1rem;
+    }
+
+    .stMetric {
+        background-color:#f5f7fa;
+        padding:15px;
+        border-radius:10px;
+    }
+
+    </style>
+    """
